c82692df1022dd544789393cc52db83fdb12be28
alycia957 <benhammaalycia@gmail.com>
2025-03-26 15:33:27 +0100

    voilaaaaaa

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/user.js');
-router.post('/register', userController.register);
+
 const router = express.Router();
 const { 
     registerUser, 
